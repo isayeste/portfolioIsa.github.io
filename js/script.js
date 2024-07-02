53861178b2b17831985c4156f12f472138365468
iyessan445 <143215657+isayeste@users.noreply.github.com>
2024-07-03 00:20:07 +0200

    portfolio

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const menu = document.getElementById("btn-abrir-menu");
-        menu.addEventListener('click', () => {
-        console.log('Clic en el botón'); 
-        document.getElementById('contenedor-menu').classList.toggle('active');
-        });
